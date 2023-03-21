724d1581f9eca667608ddbfd6a01b624e3375f11
Tor <tor.devries@wsu.edu>
2023-03-21 14:38:36 +0000

    first commit to the repository

---
+++ scripts.js
@@ -0,0 +1,20 @@
+/* javascript for TP10 */
+
+// add some code to wait for the DOM
+document.addEventListener("DOMContentLoaded", function() {
+
+    // code to load API
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'b1191f052bmsh9393381bd6d8022p103498jsna1764183a67a',
+            'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+        }
+    };
+    
+    fetch('https://weatherapi-com.p.rapidapi.com/current.json?q=99163', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+
+});
